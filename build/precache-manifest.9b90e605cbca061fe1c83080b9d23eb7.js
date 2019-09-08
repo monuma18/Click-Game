121181f75f75b8a67c0dd52005f8c09fa3b4f276
Charles Monuma <monuma18@gmail.com>
2019-09-07 19:15:18 -0500

    update

---
+++ build/precache-manifest.9b90e605cbca061fe1c83080b9d23eb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3060cca5e9c7288cb5dc239fecd5ae11",
+    "url": "/Click-Game/index.html"
+  },
+  {
+    "revision": "f3974014315494562987",
+    "url": "/Click-Game/static/css/main.cf09678b.chunk.css"
+  },
+  {
+    "revision": "5e83aeb0a3d3423cfe0e",
+    "url": "/Click-Game/static/js/2.80f08b19.chunk.js"
+  },
+  {
+    "revision": "f3974014315494562987",
+    "url": "/Click-Game/static/js/main.8410ee67.chunk.js"
+  },
+  {
+    "revision": "89c5d5001c48a11985a1",
+    "url": "/Click-Game/static/js/runtime~main.e02aa7a2.js"
+  }
+]);
